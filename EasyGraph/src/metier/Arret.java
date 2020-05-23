a36f03ad715029d837b38315933b1e54ec84f6ec
MouadKH9 <mouad.khchich@gmail.com>
2020-05-23 14:34:42 +0000

    Added Icon and fixed cout's position

--- EasyGraph/src/metier/Arret.java
+++ EasyGraph/src/metier/Arret.java
@@ -123,52 +123,29 @@ public class Arret implements Comparable<Arret>,Serializable {
         g2d.setStroke(st);
     }
     public void drawLabel(Graphics2D g2d){
-        if(Configuration.pondere){
-            if(Configuration.oriente){
-                double dx = sommetB.getPosition_x() - sommetA.getPosition_x();
-                double dy = sommetB.getPosition_y() - sommetA.getPosition_y();
-                double distance = Math.sqrt(Math.pow(dx, 2)+Math.pow(dy, 2));
-
-                double x_arrow = ((Configuration.taille_sommet/distance)*(sommetA.getPosition_x()-sommetB.getPosition_x())) + sommetB.getPosition_x();
-                double y_arrow = ((Configuration.taille_sommet/distance)*(sommetA.getPosition_y()-sommetB.getPosition_y())) + sommetB.getPosition_y();
-                drawCout(g2d,sommetA.getPosition_x(),sommetA.getPosition_y(),x_arrow,y_arrow);
-            }else if(sommetA!=sommetB){
-                //get center of the line
-                int centerX =sommetA.getPosition_x() + ((sommetB.getPosition_x()-sommetA.getPosition_x())/2);
-                int centerY =sommetA.getPosition_y() + ((sommetB.getPosition_y()-sommetA.getPosition_y())/2);
-                //get the angle in degrees
-                double deg = Math.toDegrees(Math.atan2(centerY - sommetB.getPosition_y(), centerX - sommetB.getPosition_x())+ Math.PI);
-                //need this in order to flip the text to be more readable within angles 90<deg<270
-                if ((deg>90)&&(deg<270)){
-                    deg += 180;
-                }
-                double angle = Math.toRadians(deg);
-                String label=""+cout;
-                if(flux!=null)label+="/"+flux;
-                //get the length of the text on screen
-                int sw =  g2d.getFontMetrics().stringWidth(label);
-                //rotate the text
-                g2d.rotate(angle, centerX, centerY);
-                //draw the text to the center of the line
-                g2d.setColor(Configuration.coleur_label);
-                //if(flux!=0)label=
-                g2d.drawString(label, centerX - (sw/2), centerY - (Configuration.taille_arret+5)); 
-                //reverse the rotation
-                g2d.rotate(-angle, centerX, centerY);
-            }
+        if(Configuration.pondere && sommetA!=sommetB){
+        	//get center of the line
+            int centerX =sommetA.getPosition_x() + ((sommetB.getPosition_x()-sommetA.getPosition_x())/2);
+            int centerY =sommetA.getPosition_y() + ((sommetB.getPosition_y()-sommetA.getPosition_y())/2);
+            //get the angle in degrees
+            double deg = Math.toDegrees(Math.atan2(centerY - sommetB.getPosition_y(), centerX - sommetB.getPosition_x())+ Math.PI);
+            //need this in order to flip the text to be more readable within angles 90<deg<270
+            if ((deg>90)&&(deg<270))
+                deg += 180;
+            double angle = Math.toRadians(deg);
+            String label = "" + cout;
+            if(flux!=null) label += "/"+flux;
+            //get the length of the text on screen
+            int sw =  g2d.getFontMetrics().stringWidth(label);
+            //rotate the text
+            g2d.rotate(angle, centerX, centerY);
+            //draw the text to the center of the line
+            g2d.setColor(Configuration.coleur_label);
+            //if(flux!=0)label=
+            g2d.drawString(label, centerX - (sw/2), centerY - (Configuration.taille_arret+5)); 
+            //reverse the rotation
+            g2d.rotate(-angle, centerX, centerY);
         }
-    }
-    private void drawCout(Graphics2D g2d,int x1,int y1, double x2, double y2){
-        double dy = y2 - y1;
-        double dx = x2 - x1;
-        double theta = Math.atan2(dy, dx);
-        double x = 0, y = 0, rho = theta + Math.toRadians(15);
-        x = x2 - 15 * Math.cos(rho);
-        y = y2 - 15 * Math.sin(rho);
-        g2d.setColor(Configuration.coleur_label);
-        String label=""+cout;
-        if(flux!=null)label+="/"+flux;
-        g2d.drawString(label, (int)x-10, (int)y-10);
     }
     private void drawArrowHead(Graphics2D g2d,int x1,int y1, double x2, double y2)
     {
